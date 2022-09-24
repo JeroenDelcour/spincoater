1f2efadaaa33f313afd275c687ccc84d338b2b73
Jeroen Delcour <jeroen@track32.nl>
2022-09-24 13:12:44 +0200

    add button, add timer countdown, GUI tweaks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, Timer
 import time
 import uasyncio
 
@@ -10,34 +10,37 @@ i2c = I2C(1, sda=Pin(21), scl=Pin(22))
 display = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 
-def splash():
-    display.fill(0)
-    display.fill_rect(0, 0, 32, 32, 1)
-    display.fill_rect(2, 2, 28, 28, 0)
-    display.vline(9, 8, 22, 1)
-    display.vline(16, 2, 22, 1)
-    display.vline(23, 8, 22, 1)
-    display.fill_rect(26, 24, 2, 4, 1)
-    display.text("MicroPython", 40, 0, 1)
-    display.text("SSD1306", 40, 12, 1)
-    display.text("OLED 128x64", 40, 24, 1)
-    display.show()
+# def splash():
+#     display.fill(0)
+#     display.fill_rect(0, 0, 32, 32, 1)
+#     display.fill_rect(2, 2, 28, 28, 0)
+#     display.vline(9, 8, 22, 1)
+#     display.vline(16, 2, 22, 1)
+#     display.vline(23, 8, 22, 1)
+#     display.fill_rect(26, 24, 2, 4, 1)
+#     display.text("MicroPython", 40, 0, 1)
+#     display.text("SSD1306", 40, 12, 1)
+#     display.text("OLED 128x64", 40, 24, 1)
+#     display.show()
 
 
 def start_view(state, rotary):
+    display.text("SPIN", 34, 4, 1)
+    display.text("COATER", 50, 14, 1)
+
     before = "> " if rotary.value() == 0 else "  "
-    display.text(before + "Start", 12, 26, 1)
+    display.text(before + "Start", 12, 36, 1)
     before = "> " if rotary.value() == 1 else "  "
-    display.text(before + "Edit", 12, 36, 1)
+    display.text(before + "Edit", 12, 46, 1)
 
 
 def draw_edit_menu(state, rotary):
     display.text("Deposit speed:", 0, 0, 1)
     display.text("{: >{w}} RPM".format(state["deposit_rpm"], w=4), 62, 10, 1)
     display.text("Coating speed:", 0, 22, 1)
-    display.text("{: >{w}} RPM".format(state["spin_rpm"], w=4), 62, 32, 1)
+    display.text("{: >{w}} RPM".format(state["coating_rpm"], w=4), 62, 32, 1)
     display.text("Coating time:", 0, 44, 1)
-    display.text("{: >{w}} sec".format(state["spin_time"], w=4), 62, 54, 1)
+    display.text("{: >{w}} sec".format(state["coating_time"], w=4), 62, 54, 1)
 
 
 def edit_deposit_view(state, rotary):
@@ -46,31 +49,35 @@ def edit_deposit_view(state, rotary):
     display.text(">", 40, 10, 1)
 
 
-def edit_spin_rpm_view(state, rotary):
-    state["spin_rpm"] = rotary.value() * 10
+def edit_coating_rpm_view(state, rotary):
+    state["coating_rpm"] = rotary.value() * 10
     draw_edit_menu(state, rotary)
     display.text(">", 40, 32, 1)
 
 
-def edit_spin_time_view(state, rotary):
-    state["spin_time"] = rotary.value()
+def edit_coating_time_view(state, rotary):
+    state["coating_time"] = rotary.value()
     draw_edit_menu(state, rotary)
     display.text(">", 40, 54, 1)
 
 
 def draw_rpm(rpm):
-    display.text("RPM:{: >{w}}".format(rpm, w=5), 30, 16, 1)
+    display.text("RPM:{: >{w}}".format(rpm, w=5), 30, 27, 1)
 
 
-def deposit_view(state):
+def deposit_view(state, rotary):
+    display.fill_rect(0, 0, 127, 14, 1)
+    display.text("Deposit", 36, 3, 0)
     draw_rpm(state["rpm"])
-    display.text("Press to", 32, 38, 1)
-    display.text("continue", 32, 48, 1)
+    display.text("Press to", 32, 45, 1)
+    display.text("continue", 32, 55, 1)
 
 
-def spinning_view(state):
+def coating_view(state, rotary):
+    display.fill_rect(0, 0, 127, 14, 1)
+    display.text("Coating", 36, 3, 0)
     draw_rpm(state["rpm"])
-    display.text("{: >{w}} sec".format(state.timer, w=4), 30, 48, 1)
+    display.text("{: >{w}} sec".format(state["timer"], w=4), 30, 48, 1)
 
 
 async def update():
@@ -83,7 +90,16 @@ async def update():
         await uasyncio.sleep_ms(33)
 
 
-def on_button_press():
+def debounce_button(p):
+    p.irq(trigger=Pin.IRQ_FALLING, handler=None)  # remove irq
+    timer0 = Timer(0)
+    timer0.init(period=20, mode=Timer.ONE_SHOT, callback=lambda t: on_button_press(p))
+
+
+def on_button_press(p):
+    p.irq(trigger=Pin.IRQ_FALLING, handler=debounce_button)  # restore irq
+    if p.value() == 1:  # debounce
+        return
     global state
     global rotary
     if state["view"] == start_view:
@@ -100,54 +116,94 @@ def on_button_press():
             )
             return
     if state["view"] == edit_deposit_view:
-        state["view"] = edit_spin_rpm_view
+        state["view"] = edit_coating_rpm_view
         rotary.set(
             min_val=0,
             max_val=9990,
             range_mode=RotaryIRQ.RANGE_BOUNDED,
-            value=int(0.1 * state["spin_rpm"]),
+            value=int(0.1 * state["coating_rpm"]),
         )
         return
-    if state["view"] == edit_spin_rpm_view:
-        state["view"] = edit_spin_time_view
+    if state["view"] == edit_coating_rpm_view:
+        state["view"] = edit_coating_time_view
         rotary.set(
             min_val=0,
             max_val=9999,
             range_mode=RotaryIRQ.RANGE_BOUNDED,
-            value=state["spin_time"],
+            value=state["coating_time"],
         )
         return
-    if state["view"] == edit_spin_time_view:
-        rotary.set(min_val=0, max_val=1, range_mode=RotaryIRQ.RANGE_BOUNDED)
+    if state["view"] == edit_coating_time_view:
+        rotary.set(min_val=0, max_val=1, range_mode=RotaryIRQ.RANGE_BOUNDED, value=0)
         state["view"] = start_view
         return
     if state["view"] == deposit_view:
-        state["view"] = spinning_view
+        state["view"] = coating_view
+        start_coating(state)
+        return
+    if state["view"] == coating_view:
+        stop_coating()
         return
 
 
-splash()
-time.sleep(1)
+timer1 = Timer(1)
+timer2 = Timer(2)
+
+
+def start_coating(state):
+    global timer1
+    global timer2
+
+    state["timer"] = state["coating_time"]
+
+    timer1.init(
+        period=state["coating_time"] * 1000,
+        mode=Timer.ONE_SHOT,
+        callback=lambda t: stop_coating(),
+    )
+
+    def decrement_timer(t):
+        state["timer"] -= 1
+
+    timer2.init(period=1000, mode=Timer.PERIODIC, callback=decrement_timer)
+
+
+def stop_coating():
+    global state
+    global rotary
+    global timer1
+    global timer2
+    timer1.deinit()
+    timer2.deinit()
+    state["timer"] = 0
+    rotary.set(min_val=0, max_val=1, range_mode=RotaryIRQ.RANGE_BOUNDED, value=0)
+    state["view"] = start_view
+
+
+# splash()
+# time.sleep(1)
 
 rotary = RotaryIRQ(
-    pin_num_clk=12,
+    pin_num_clk=14,
     pin_num_dt=13,
     min_val=0,
-    max_val=500,
+    max_val=1,
     range_mode=RotaryIRQ.RANGE_BOUNDED,
     pull_up=True,
 )
 
+button = Pin(19, Pin.IN, Pin.PULL_UP)
+button.irq(trigger=Pin.IRQ_FALLING, handler=on_button_press)
+
 state = {
-    "view": edit_spin_time_view,
+    "view": start_view,
     "rpm": 0,
     "timer": 0,
     "deposit_rpm": 500,
-    "spin_rpm": 6000,
-    "spin_time": 120,
+    "coating_rpm": 6000,
+    "coating_time": 10,
 }
 
 event_loop = uasyncio.get_event_loop()
 event_loop.create_task(update())
-# event_loop.create_task(user_input())
 event_loop.run_forever()
