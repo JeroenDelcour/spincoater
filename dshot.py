5108886126bdefc96dc2f75e18e074ca54788c0b
Jeroen Delcour <jeroen@track32.nl>
2022-10-15 10:38:06 +0200

    splash screen, failed attempts at getting telemetry

--- dshot.py
+++ dshot.py
@@ -15,10 +15,12 @@ class Dshot:
         self._ON_PULSES = 57  # number of pulses for an ON bit
         self._OFF_PULSES = 29  # number of pulses for an OFF bit
         self._BIT_PULSES = 76  # total number of pulses per bit
+        self._PAUSE_PULSES = 21  # pause after sending packet
 
         self.rmt = RMT(0, pin=pin, clock_div=7)
 
         self._arm()
+        self._enable_telemetry()
 
     def _arm(self, duration=3):
         """
@@ -32,10 +34,43 @@ class Dshot:
             self.set_throttle(0)
             # time.sleep(0.01)
 
-    def set_throttle(self, value: float, telemetry=True):
-        """
-        Set throttle to a value between 0 and 1.
-        """
+    def _enable_telemetry(self):
+        # for i in range(6):
+        #     value = self._create_packet(13)
+        #     print(bin(value))
+        #     self._send(value)
+        for i in range(6):
+            value = self._create_packet(34)
+            print(bin(value))
+            self._send(value)
+        # for i in range(6):
+        #     value = self._create_packet(34)
+        #     print(bin(value))
+        #     self._send(value)
+        # for i in range(6):
+        #     value = self._create_packet(35)
+        #     print(bin(value))
+        #     self._send(value)
+        for i in range(6):
+            value = self._create_packet(12)
+            print(bin(value))
+            self._send(value)
+        time.sleep(0.040)
+
+        # self._send(self._create_packet(42))
+        # self._send(self._create_packet(43))
+        # self._send(self._create_packet(44))
+        # self._send(self._create_packet(45))
+        # self._send(self._create_packet(46))
+        # self._send(self._create_packet(47))
+
+        # beep
+        # time.sleep(0.050)
+        # for i in range(5):
+        #     self._send(self._create_packet(i + 1))
+        #     time.sleep(0.250)
+
+    def _encode_throttle(self, value):
         # 11 bit throttle: 2048 possible values.
         # 0 is reserved for disarmed. 1-47 are reserved for special commands.
         # Leaving 48 to 2047 (2000 steps) for the actual throttle value
@@ -44,15 +79,25 @@ class Dshot:
             value = 0  # disarmed
         else:
             value = int(value * 2000 + 47)
+        return value
 
+    def set_throttle(self, value: float, telemetry=True):
+        """
+        Set throttle to a value between 0 and 1.
+        """
+        value = self._encode_throttle(value)
+        value = self._create_packet(value)
+        self._send(value)
+
+    def _create_packet(self, value, telemetry=True):
         # add telemetry bit
-        value = (value << 1) + telemetry
+        value = (value << 1) | telemetry
 
         # add CRC (Cyclic Redundancy Check)
         crc = (value ^ (value >> 4) ^ (value >> 8)) & 0x0F
-        value = (value << 4) + crc
+        value = (value << 4) | crc
 
-        self._send(value)
+        return value
 
     def _send(self, value):
         """
@@ -61,10 +106,12 @@ class Dshot:
         duration = []
 
         for i in reversed(range(16)):
-            bit = (value & (2**i)) >> i  # select bit
+            bit = (value & (2**i)) == 2**i  # select bit
             if bit == 1:
                 duration += [self._ON_PULSES, self._BIT_PULSES - self._ON_PULSES]
             else:
                 duration += [self._OFF_PULSES, self._BIT_PULSES - self._OFF_PULSES]
+
+        duration[-1] += self._PAUSE_PULSES
 
         self.rmt.write_pulses(duration, True)
--- main.py
+++ main.py
@@ -12,18 +12,23 @@ display = ssd1306.SSD1306_I2C(128, 64, i2c)
 display.rotate(0)
 
 
-# def splash():
-#     display.fill(0)
-#     display.fill_rect(0, 0, 32, 32, 1)
-#     display.fill_rect(2, 2, 28, 28, 0)
-#     display.vline(9, 8, 22, 1)
-#     display.vline(16, 2, 22, 1)
-#     display.vline(23, 8, 22, 1)
-#     display.fill_rect(26, 24, 2, 4, 1)
-#     display.text("MicroPython", 40, 0, 1)
-#     display.text("SSD1306", 40, 12, 1)
-#     display.text("OLED 128x64", 40, 24, 1)
-#     display.show()
+def splash():
+    # display.fill(0)
+    # display.fill_rect(0, 0, 32, 32, 1)
+    # display.fill_rect(2, 2, 28, 28, 0)
+    # display.vline(9, 8, 22, 1)
+    # display.vline(16, 2, 22, 1)
+    # display.vline(23, 8, 22, 1)
+    # display.fill_rect(26, 24, 2, 4, 1)
+    # display.text("MicroPython", 40, 0, 1)
+    # display.text("SSD1306", 40, 12, 1)
+    # display.text("OLED 128x64", 40, 24, 1)
+
+    display.fill(0)
+    display.text("SPIN", 34, 4, 1)
+    display.text("COATER", 50, 14, 1)
+
+    display.show()
 
 
 def start_view(state, rotary):
@@ -83,9 +88,10 @@ def coating_view(state, rotary):
 
 
 def handle_ESC_telemetry(data):
-    # if data is None:
-    #     return
-    print(data)
+    if data is None:
+        return
+    print("Telemetry: ", data)
+    pass
 
 
 async def update():
@@ -197,7 +203,7 @@ def stop_coating():
     state["view"] = start_view
 
 
-# splash()
+splash()
 # time.sleep(1)
 
 rotary = RotaryIRQ(
